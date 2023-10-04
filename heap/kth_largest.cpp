bd8d30c93ee985c8097a72910a2819e2157f4c18
Charlie <charlieliu1016@gmail.com>
2023-10-04 12:51:19 -0400

    heap

---
+++ heap/kth_largest.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution{
+public:
+    int findKthLargest(vector<int> &nums, int k) {
+        priority_queue<int> pq;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            pq.push(nums[i]);
+        }
+        k--;
+        while (k--) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
